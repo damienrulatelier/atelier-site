154fa81e78b7b985a2a4c01a6eff8e61a614733d
Damien Rul <damienrul34@gmail.com>
2026-07-09 18:27:58 +0200

    vfff

--- app/components/ScrollToTop.tsx
+++ app/components/ScrollToTop.tsx
@@ -1,14 +1,12 @@
 "use client";
-
 import { useEffect } from "react";
 import { usePathname } from "next/navigation";
-
 export default function ScrollToTop() {
   const pathname = usePathname();
-
   useEffect(() => {
-    window.scrollTo(0, 0);
+    if (typeof window !== "undefined") {
+      setTimeout(() => window.scrollTo(0, 0), 0);
+    }
   }, [pathname]);
-
   return null;
 }
